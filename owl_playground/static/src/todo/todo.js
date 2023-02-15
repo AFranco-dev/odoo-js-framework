ff09850b7f17e93cf9ce291e0a1a4fa768b4feec
Andre Franco <andre.franco.dev@gmail.com>
2023-02-15 16:44:16 -0600

    trying to separete component files

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -11,8 +11,8 @@ export class Todo extends Component {
   }
 }
 Todo.template = "owl_playground.Todo";
-Todo.props = {
-  id: { type: Number },
-  description: { type: String },
-  done: { type: Boolean },
-};
+// Todo.props = {
+//   id: { type: Number },
+//   description: { type: String },
+//   done: { type: Boolean },
+// };
