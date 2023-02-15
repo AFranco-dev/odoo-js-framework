2a42694b88413b065776df40d83e1262e71347c7
Andre Franco <andre.franco.dev@gmail.com>
2023-02-15 15:09:47 -0600

    trying to separete component files

--- owl_playground/static/src/counter.js
+++ owl_playground/static/src/counter.js
@@ -3,13 +3,7 @@
 import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
-  static template = xml`
-        <div class="p-3">
-            <p>
-                Counter <t t-esc="state.value" />
-            </p>
-            <button class="btn btn-primary" t-on-click="increment">Increment</button>
-        </div>`;
+  static template = "owl_playground.counter";
   state = useState({ value: 0 });
   increment() {
     this.state.value++;
