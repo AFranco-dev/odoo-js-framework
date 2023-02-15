c1d913ae0d9fb3d8713d3f7acd8aa4de0a75ad2d
Andre Franco <andre.franco.dev@gmail.com>
2023-02-15 14:48:21 -0600

    trying to separete component files

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,11 +1,9 @@
 /** @odoo-module **/
 
 import { Component, useState } from "@odoo/owl";
+import { Counter } from "./counter";
 
 export class Playground extends Component {
   static template = "owl_playground.playground";
-  state = useState({ value: 0 });
-  increment() {
-    this.state.value++;
-  }
+  static components = { Counter };
 }
