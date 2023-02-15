bfe198a4a80b96953df5d1a9f96bba0791257e0b
Andre Franco <andre.franco.dev@gmail.com>
2023-02-15 16:32:34 -0600

    trying to separete component files

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -4,15 +4,15 @@ import { Component, useState } from "@odoo/owl";
 
 export class Todo extends Component {
   setup() {
-    this.todo = { id: 3, description: "But Milk", done: false };
+    this.todo = { id: 3, description: "Buy Milk", done: false };
   }
   increment() {
     this.state.value++;
   }
 }
-Todo.template = "owl_playground.todo";
-// Todo.props = {
-//   id: { type: Number },
-//   description: { type: String },
-//   done: { type: Boolean },
-// };
+Todo.template = "owl_playground.Todo";
+Todo.props = {
+  id: { type: Number },
+  description: { type: String },
+  done: { type: Boolean },
+};
