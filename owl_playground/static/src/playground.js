58ec4542d65973bb7dce31f6748ab30fa2e8051f
Andre Franco <andre.franco.dev@gmail.com>
2023-02-15 14:23:26 -0600

    added counter on js

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -3,5 +3,9 @@
 import { Component, useState } from "@odoo/owl";
 
 export class Playground extends Component {
-    static template = "owl_playground.playground";
+  static template = "owl_playground.playground";
+  state = useState({ value: 0 });
+  increment() {
+    this.state.value++;
+  }
 }
