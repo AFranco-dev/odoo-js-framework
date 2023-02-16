3131cab63b1ede1aea12df1473745f1b7cd6f97f
Andre Franco <andre.franco.dev@gmail.com>
2023-02-16 08:50:48 -0600

    adding prop validation

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -4,15 +4,16 @@ import { Component, useState } from "@odoo/owl";
 
 export class Todo extends Component {
   setup() {
-    this.todo = { id: 3, description: "Buy Milk", done: false };
+    this.todo = { id: 3, description: "Buy Milk" };
+    // , done: false
   }
   increment() {
     this.state.value++;
   }
 }
 Todo.template = "owl_playground.todo";
-// Todo.props = {
-//   id: { type: Number },
-//   description: { type: String },
-//   done: { type: Boolean },
-// };
+Todo.props = {
+  id: { type: Number },
+  description: { type: String },
+  done: { type: Boolean },
+};
