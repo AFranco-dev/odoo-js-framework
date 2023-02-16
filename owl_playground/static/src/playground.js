671e0887874224c4f9bbd530f139a5bb71f1e8c1
Andre Franco <andre.franco.dev@gmail.com>
2023-02-16 15:44:10 -0600

    adding prop validation

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -2,19 +2,20 @@
 
 import { Component, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { TodoList } from "./todoList/todoList";
 import { Todo } from "./todo/todo";
 
 export class Playground extends Component {
-  setup() {
-    this.state = useState({
-      todos: [
-        { id: 1, description: "Buy Milk", done: false },
-        { id: 2, description: "Clean the house", done: true },
-        { id: 3, description: "Go for a run", done: false },
-      ],
-    });
-    // console.log(state.todos);
-  }
+  // setup() {
+  //   this.state = useState({
+  //     todos: [
+  //       { id: 1, description: "Buy Milk", done: false },
+  //       { id: 2, description: "Clean the house", done: true },
+  //       { id: 3, description: "Go for a run", done: false },
+  //     ],
+  //   });
+  //   // console.log(state.todos);
+  // }
   static template = "owl_playground.playground";
-  static components = { Counter, Todo };
+  static components = { Counter, TodoList, Todo };
 }
---
+++ owl_playground/static/src/todoList/todoList.js
@@ -0,0 +1,19 @@
+/** @odoo-module **/
+
+import { Component, useState } from "@odoo/owl";
+import { Todo } from "./todo/todo";
+
+export class TodoList extends Component {
+  setup() {
+    this.state = useState({
+      todos: [
+        { id: 1, description: "Buy Milk", done: false },
+        { id: 2, description: "Clean the house", done: true },
+        { id: 3, description: "Go for a run", done: false },
+      ],
+    });
+    // console.log(state.todos);
+  }
+  static template = "owl_playground.todoList";
+  static components = { Todo };
+}
