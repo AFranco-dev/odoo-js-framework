f15acee2f6a943cdf11c3816cc15ebb3bde7e8ed
Andre Franco <andre.franco.dev@gmail.com>
2023-02-16 14:23:43 -0600

    adding prop validation

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -11,7 +11,7 @@ export class Todo extends Component {
         shape: {
           id: Number,
           description: String,
-          done: Boolean,
+          done: { type: Boolean, default: false },
         },
       },
     },
