09d655da209f8c65baa745c32e1bf38cfcb6e5ea
Andre Franco <andre.franco.dev@gmail.com>
2023-02-16 09:59:36 -0600

    adding prop validation

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -5,9 +5,6 @@ import { Counter } from "./counter/counter";
 import { Todo } from "./todo/todo";
 
 export class Playground extends Component {
-  setup() {
-    this.todo = { id: 3, description: "Buy Milk", done: false };
-  }
   static template = "owl_playground.playground";
   static components = { Counter, Todo };
 }
