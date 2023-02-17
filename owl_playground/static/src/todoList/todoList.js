25ab698f386115cc4e14314aeb78371b0eb9591b
Andre Franco <andre.franco.dev@gmail.com>
2023-02-17 08:54:25 -0600

    added logic for adding extra todos

--- owl_playground/static/src/todoList/todoList.js
+++ owl_playground/static/src/todoList/todoList.js
@@ -12,7 +12,17 @@ export class TodoList extends Component {
         { id: 3, description: "Go for a run", done: false },
       ],
     });
+    this.nextId = 0;
     // console.log(state.todos);
+  }
+  addTodo(e) {
+    if (ev.keyCode === 13 && ev.target.value != "") {
+      this.state.todos.push({
+        id: this.nextId++,
+        description: ev.target.value,
+        done: false,
+      });
+    }
   }
   static template = "owl_playground.todoList";
   static components = { Todo };
