62724058a6c06cb87a26348488b376fda8e3f843
Andre Franco <andre.franco.dev@gmail.com>
2023-02-17 16:22:43 -0600

    first parte of the second part of the js framework tutorial

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,12 +1,28 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
 
-const { Component } = owl;
+const { Component, useSubEnv } = owl;
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+  setup() {
+    // The useSubEnv below can be deleted if you're > 16.0
+    useSubEnv({
+      config: {
+        ...getDefaultConfig(),
+        ...this.env.config,
+      },
+    });
 
-AwesomeDashboard.components = {};
+    this.display = {
+      controlPanel: { "top-right": false, "bottom-right": false },
+    };
+  }
+}
+
+AwesomeDashboard.components = { layout };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
