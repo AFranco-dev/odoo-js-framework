bffebf13aa433a6daee0a9ae1b12029cce5476c7
Andre Franco <andre.franco.dev@gmail.com>
2023-02-18 09:14:42 -0600

    added buttons with actions and domains

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -26,6 +26,28 @@ class AwesomeDashboard extends Component {
   openCustomerView() {
     this.action.doAction("base.action_partner_form");
   }
+  openOrders(title, domain) {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: title,
+      res_model: "awesome_tshirt.order",
+      domain: new Domain(domain).toList(),
+      views: [
+        [false, "list"],
+        [false, "form"],
+      ],
+    });
+  }
+  openLast7DaysOrders() {
+    const domain =
+      "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+    this.openOrders("Last 7 days orders", domain);
+  }
+  openLast7DaysCancelledOrders() {
+    const domain =
+      "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
+    this.openOrders("Last 7 days orders", domain);
+  }
 }
 
 AwesomeDashboard.components = { Layout };
