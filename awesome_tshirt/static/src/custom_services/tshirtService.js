530f20c412f931b8b87e7f0c83e17fe498b38d9f
Andre Franco <andre.franco.dev@gmail.com>
2023-02-20 09:38:06 -0600

    fixed service not being added properly due to wrong syntaxis on function

--- awesome_tshirt/static/src/custom_services/tshirtService.js
+++ awesome_tshirt/static/src/custom_services/tshirtService.js
@@ -12,4 +12,4 @@ export const tshirtService = {
     };
   },
 };
-registry.category("services").add("tshirtService", "tshirtService");
+registry.category("services").add("tshirtService", tshirtService);
